658462a0d778fdd7226525fe235a2c5811407d44
tamir99 <tamirtamir20@gmail.com>
2022-11-27 22:33:33 +0200

    add connection to db

--- my api server/app.py
+++ my api server/app.py
@@ -2,10 +2,73 @@ from flask import Flask
 import json 
 from myTempDB import *  
 from flask_cors import CORS
+from pymongo import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def hello_world():
-    return json.dumps(dictionary)
+    # return json.dumps(dictionary)
+   return get_myMatchesBets()
+
+def get_collection(collectionName):
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb://localhost:27017"
+   DB_NAME = "WorldCupProject"
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+   db = client[DB_NAME]
+   collection = db[collectionName]
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return collection
+
+def myFind(collection,query):
+    cursor = get_collection(collection).find(query)
+    list_cur = list(cursor)
+    json_data = json.loads(json_util.dumps(list_cur, indent = 2) )
+    return(json_data)
+
+def get_myMatchesBets():
+    matches = myFind(collection="Matches", query="")
+
+    # get player bets for each match
+    for match in matches:
+        mybets = myFind(collection="Bets", query={"matchID": match["matchID"]})[0]
+        match["countryABet"] = mybets["countryABet"]
+        match["countryBet"] = mybets["countryBet"]      
+  
+    # get countries A flags for each match
+    for match in matches:
+        country = myFind(collection="Countries", query={"countryLabel": match["countryALabel"]})[0]
+        match["countryAImage"] = country["countryImage"]
+
+    # get countries B flags for each match
+    for match in matches:
+        country = myFind(collection="Countries", query={"countryLabel": match["countryBLabel"]})[0]
+        match["countryBImage"] = country["countryImage"]
+
+    # get player points for each match
+    for match in matches:
+        mybets = myFind(collection="Bets", query={"matchID": match["matchID"]})[0]
+        countryAScore = match["countryAScore"]
+        countryBScore = match["countryBScore"]
+        countryABet = mybets["countryABet"]
+        countryBBet = mybets["countryBet"]
+        points = 0
+        if countryABet == countryAScore and countryBBet == countryBScore:
+            points = 3
+        elif countryABet - countryBBet == countryAScore - countryBScore:
+            points = 2
+        elif countryABet > countryBBet and countryAScore > countryBScore:
+            points = 1
+        elif countryABet < countryBBet and countryAScore < countryBScore:
+            points = 1
+        elif countryABet == countryBBet and countryAScore == countryBScore:
+            points = 1
+        match["points"] = points
+        
+    return(json.dumps(matches))
+
+# get_myMatchesBets()   
--- my api server/myTempDB.py
+++ my api server/myTempDB.py
@@ -1,4 +1,47 @@
+# dictionary = [
+#   {"name": "A", "points": 1} , 
+#   {"name": "B", "points": 2}
+# ]
+
 dictionary = [
-  {"name": "A", "points": 1} , 
-  {"name": "B", "points": 2}
+    {
+        "matchID": 1,
+        "countryALabel": "Israel",
+        "countryAImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Flag_of_Israel.svg/800px-Flag_of_Israel.svg.png",
+        "groupTitle": "Group A",
+        "countryABet": 1,
+        "countryAScore": 1,
+        "points": 0,
+        "countryBScore": 0,
+        "countryBet": 2,
+        "countryBImage": "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/125px-Flag_of_Brazil.svg.png",
+        "countryBLabel": "Brazil"
+    },
+    {
+        "matchID": 2,
+        "countryALabel": "England",
+        "countryAImage": "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/125px-Flag_of_England.svg.png",
+        "groupTitle": "Group B",
+        "countryABet": 3,
+        "countryAScore": 2, 
+        "points": 2,
+        "countryBScore": 2,
+        "countryBet": 3, 
+        "countryBImage": "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/125px-Flag_of_Germany.svg.png",
+        "countryBLabel": "Germany"
+    },
+    {
+        "matchID": 3,
+        "countryALabel": "Japan",
+        "countryAImage": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/125px-Flag_of_Japan.svg.png",
+        "groupTitle": "Group A",
+        "countryABet": 4,
+        "countryAScore": 4, 
+        "points": 3,
+        "countryBScore": 2,
+        "countryBet": 2, 
+        "countryBImage": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/125px-Flag_of_the_United_States.svg.png",
+        "countryBLabel": "USA"
+    }
+
 ]
